8d7164a1bc0754b89d09027c0868425e1b229818
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-06-28 10:27:14 +0200

    completed function exercise 3

--- index.js
+++ index.js
@@ -67,11 +67,17 @@ dice()
 
 const whoIsBigger = (a, b) => a > b ? a : b
 
-console.log(whoIsBigger(40, 7))
+whoIsBigger(40, 7)
+
+
 /* Ex.3
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
+
+const splitMe = (str) => str.split(" ")
+
+splitMe("Strive is excellent")
 
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.

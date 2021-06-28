4bbfb3247a5445dc72f18e2c0fa4180105abc8d2
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-06-28 09:55:15 +0200

    completed exercise a,b,c

---
+++ index.js
@@ -0,0 +1,36 @@
+//JS Basics
+
+/* Ex.A
+   Create a variable called "test" and assign a string to it.
+*/
+
+let test = "Strive";
+
+/* Ex.B
+    Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
+*/
+
+let sum = 10 + 20
+
+/* Ex.C
+    Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+*/
+
+let random = Math.floor(Math.random() * 21)
+
+
+/* Ex.D
+    Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+*/
+
+/* Ex.E
+    Programmatically remove the age property from the previously create object.
+*/
+
+/* Ex.F
+   Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
+*/
+
+/* Ex.G
+   Programmatically remove the last skill from the "skills" array inside the "me" object.
+*/
